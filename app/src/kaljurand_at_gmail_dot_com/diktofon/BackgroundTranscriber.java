38210bfddb21b835d6e88ad9207a13b17da6a967
Kaarel Kaljurand <kaljurand@gmail.com>
2011-11-27 10:11:44 +0100

    Changed the copyright owner
        Kaarel Kaljurand -> Institute of Cybernetics at Tallinn University of Technology

--- app/src/kaljurand_at_gmail_dot_com/diktofon/BackgroundTranscriber.java
+++ app/src/kaljurand_at_gmail_dot_com/diktofon/BackgroundTranscriber.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Kaarel Kaljurand
+ * Copyright 2011, Institute of Cybernetics at Tallinn University of Technology
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
